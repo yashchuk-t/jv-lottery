0c5ea9c3a0c2adb189a4d829c42ff0e982ccb4b7
Tetiana Yashchuk <yashchuk.t.u@gmail.com>
2026-05-10 23:07:21 +0300

    fix: follow review requirements (remove static, add constants)

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static String getRandomColor() {
+    private final Random random = new Random();
+
+    public String getRandomColor() {
+        Color[] colors = Color.values();
         int index = new Random().nextInt(Color.values().length);
-        Color color = Color.values()[index];
-        return color.toString();
+        Color color = colors[index];
+        return color.name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        String color = ColorSupplier.getRandomColor();
-        int number = new Random().nextInt(100) + 1;
+        String color = colorSupplier.getRandomColor();
+        int number = random.nextInt(MAX_BALL_NUMBER);
         return new Ball(color, number);
     }
 }
