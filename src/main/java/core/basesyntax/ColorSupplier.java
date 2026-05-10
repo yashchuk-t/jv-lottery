edd5b181e892d385276d4f74d00234bfdd8a3b83
Tetiana Yashchuk <yashchuk.t.u@gmail.com>
2026-05-10 23:58:52 +0300

    fix: added an empty lines

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
+
 import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
+
     public String getRandomColor() {
         Color[] colors = Color.values();
         int index = random.nextInt(colors.length);
