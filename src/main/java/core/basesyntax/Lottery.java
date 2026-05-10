46dfa794278ff137814d9ea6f9e3a9d3803c8f21
Tetiana Yashchuk <yashchuk.t.u@gmail.com>
2026-05-10 23:52:59 +0300

    fix: added an empty line

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ public class Lottery {
     private static final int MAX_BALL_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
         int number = random.nextInt(MAX_BALL_NUMBER + 1);
