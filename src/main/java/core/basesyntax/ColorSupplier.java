ed099eea4e3ad90ba987fc18eb75d88504e24f90
Tetiana Yashchuk <yashchuk.t.u@gmail.com>
2026-05-10 23:46:43 +0300

    fix: Removed redundant empty lines

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,14 +1,11 @@
 package core.basesyntax;
-
 import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
-
     public String getRandomColor() {
         Color[] colors = Color.values();
-        int index = new Random().nextInt(Color.values().length);
-        Color color = colors[index];
-        return color.name();
+        int index = random.nextInt(colors.length);
+        return colors[index].name();
     }
 }
